dd9c5f137ad10232ac128395e021d52f741b8be9
Sarankumar S <saransekar94@gmail.com>
2023-09-25 15:11:07 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,12 @@
+provider "aws"{
+  region = "ap-south-1"
+} 
+resource "aws_instance" "devops"{
+  ami           = "ami-06a0b4e3b7eb7a300"
+  instance_type = "t2.micro"
+  key_name = "checkkp"
+
+  tags = {
+    Name = "terraform"
+    }
+  }
